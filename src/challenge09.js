8ffeb39d3befc06f8377810ea3474a69984964e1
Pablo Jesús Pedraza <wensel84@gmail.com>
2023-12-26 10:33:40 -0300

    el 9

--- src/challenge09.js
+++ src/challenge09.js
@@ -1,38 +1,37 @@
 function adjustLights(lights) {
   const [firstColor] = lights
   let countWrong = 0
-  let countWrong2 = 0
+  let countWrongInverted = 0
   lights
     .filter((_, index) => index % 2 === 0)
     .forEach((light) => {
       if (light !== firstColor) countWrong++
-      if (light === firstColor) countWrong2++
+      if (light === firstColor) countWrongInverted++
     })
   lights
     .filter((_, index) => index % 2 !== 0)
     .forEach((light) => {
       if (light === firstColor) countWrong++
-      if (light !== firstColor) countWrong2++
+      if (light !== firstColor) countWrongInverted++
     })
-  console.log(countWrong)
-  console.log(countWrong2)
-  return countWrong > countWrong2 ? countWrong2 : countWrong
+
+  return countWrong > countWrongInverted ? countWrongInverted : countWrong
 }
 
-//adjustLights(['🟢', '🔴', '🟢', '🟢', '🟢'])
+adjustLights(['🟢', '🔴', '🟢', '🟢', '🟢'])
 // -> 1 (cambias la cuarta luz a 🔴)
 
-//adjustLights(['🔴', '🔴', '🟢', '🔴', '🟢'])
+adjustLights(['🔴', '🔴', '🟢', '🔴', '🟢'])
 // -> 1 (cambia la primera luz a verde)
 
-//adjustLights(['🔴', '🔴', '🟢', '🟢', '🔴'])
+adjustLights(['🔴', '🔴', '🟢', '🟢', '🔴'])
 // -> 2 (cambias la segunda luz a 🟢 y la tercera a 🔴)
 
 adjustLights(['🔴', '🔴', '🟢', '🔴', '🟢'])
 // -> 1
 
-//adjustLights(['🟢', '🔴', '🟢', '🔴', '🟢'])
+adjustLights(['🟢', '🔴', '🟢', '🔴', '🟢'])
 // -> 0 (ya están alternadas)
 
-//adjustLights(['🔴', '🔴', '🔴'])
+adjustLights(['🔴', '🔴', '🔴'])
 // -> 1 (cambias la segunda luz a 🟢)
